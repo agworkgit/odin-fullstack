7114c4a63ea488b59b88ea077d9b8f9dbd7131a7
Alex Grigore <146381585+agworkgit@users.noreply.github.com>
2025-10-29 23:11:22 +0000

    passed calculator tests

--- 05_data_types/12_calculator/calculator.js
+++ 05_data_types/12_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (numArr) {
+  let result = 0;
+  for (let i = 0; i < numArr.length; i++) {
+    //console.log(numArr[i]);
+    result += numArr[i];
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function (numArr) {
+  // iterate through array
+  // take that result and multiply with index val
+  let result = 1;
+  for (let i = 0; i < numArr.length; i++) {
+    //console.log(numArr[i]);
+    result *= numArr[i];
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function (base, exp) {
+  return base ** exp;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  // init result
+  let result = 1;
+  // iterate from 1 to num
+  for (let i = 1; i <= num; i++) {
+    // multiple each consecutive value
+    result *= i;
+  }
+  // return result
+  return result;
 };
 
 // Do not edit below this line
