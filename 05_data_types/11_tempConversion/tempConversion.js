00ef7d477671175e3685c38fef3d9ffe60fa9fe1
Alex Grigore <146381585+agworkgit@users.noreply.github.com>
2025-10-29 19:43:11 +0000

    passed tempConversion

--- 05_data_types/11_tempConversion/tempConversion.js
+++ 05_data_types/11_tempConversion/tempConversion.js
@@ -1,8 +1,17 @@
-const convertToCelsius = function() {
+// °C = (°F − 32) * 5/9
+
+const convertToCelsius = function (num) {
+  return Number(((num - 32) * 5 / 9).toPrecision(3));
 };
 
-const convertToFahrenheit = function() {
+// °F = (°C * 9/5) + 32
+
+const convertToFahrenheit = function (num) {
+  return Number(((num * 9 / 5) + 32).toPrecision(4));
 };
+
+console.log(convertToCelsius(100));
+console.log(convertToFahrenheit(73.2));
 
 // Do not edit below this line
 module.exports = {
